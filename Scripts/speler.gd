83a7929bd3f810b78f7fb4997c4bf7a9ff155a2f
Thijmendamhuis <thijmendamhuis2006@gmail.com>
2024-06-23 19:38:49 +0200

    speler

--- Scripts/speler.gd
+++ Scripts/speler.gd
@@ -1,11 +1,43 @@
-extends AnimatedSprite2D
+extends CharacterBody2D
+
+class_name Speler
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+enum SpelerMode {
+	klein,
+	Normaal,
+	groot,
+}
+
+@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var area_collision_shape = $Area2D/AreaCollisionShape
+@onready var body_collision_shape = $BodyCollisionShape 
+
+@export_group("locomotion")
+@export var run_speed_damping = 0.5
+@export var speed = 200
+@export var jump_velocity = -400
+@export_group("")
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = jump_velocity
+	
+	if Input.is_action_just_released("ui_up") and velocity.y <0:
+		velocity.y *= 0.5
+		
+		
+	var direction = Input.get_axis("ui_left", "ui_right")
+	
+	if direction:
+		velocity.x = lerpf(velocity.x, speed * direction, run_speed_damping * delta)
+	else: 
+		velocity.x = move_toward(velocity.x, 0, speed * delta)
+		
+		
+	move_and_slide()
