9ee0aa701e498b38b53ab6e1de53500d23af44a3
Thijmendamhuis <thijmendamhuis2006@gmail.com>
2024-06-24 20:40:10 +0200

    dead

--- Scripts/speler.gd
+++ Scripts/speler.gd
@@ -3,15 +3,18 @@ extends CharacterBody2D
 class_name Speler
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 enum SpelerMode {
-	klein,
-	Normaal,
-	groot,
+	SMALL,
+	NORMAL,
+	BIG,
 }
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var area_collision_shape = $Area2D/AreaCollisionShape
-@onready var body_collision_shape = $BodyCollisionShape 
+@onready var body_collision_shape = $BodyCollisionShape
+@onready var area_2d = $Area2D
+
 
 @export_group("locomotion")
 @export var run_speed_damping = 0.5
@@ -24,48 +27,64 @@ enum SpelerMode {
 @export var max_dood_degree = 145
 @export var dood_y_velocity = -150
 @export_group("")
+
+var player_mode = SpelerMode.SMALL
+var is_dead = false
+
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = jump_velocity
-	
-	if Input.is_action_just_released("ui_up") and velocity.y <0:
+
+	if Input.is_action_just_released("ui_up") and velocity.y < 0:
 		velocity.y *= 0.5
-		
-		
+
 	var direction = Input.get_axis("ui_left", "ui_right")
-	
-	if direction:
-		velocity.x = lerpf(velocity.x, speed * direction, run_speed_damping * delta)
-	else: 
+
+	if direction != 0:
+		velocity.x = lerp(velocity.x, speed * direction, run_speed_damping * delta)
+	else:
 		velocity.x = move_toward(velocity.x, 0, speed * delta)
-		
 
 	move_and_slide()
-
 
 func _on_area_2d_area_entered(area):
 	if area is Enemy:
 		handle_enemy_collision(area)
-		
+
 func handle_enemy_collision(enemy: Enemy):
-	if enemy == null:
+	if enemy == null or is_dead:
 		return
 
-	var angle_of_collision = rad_to_deg(position.angle_to_point(enemy.position))
-	
-	if angle_of_collision > min_dood_degree && max_dood_degree > angle_of_collision:
+	var angle_of_collision = rad_to_deg((enemy.global_position - global_position).angle())
+
+	if angle_of_collision >= min_dood_degree and angle_of_collision <= max_dood_degree:
 		enemy.die()
 		on_enemy_stopped()
 	else:
 		die()
-		
-		
+
 func on_enemy_stopped():
 	velocity.y = dood_y_velocity
-	
+
 func die():
-	print("die")
-		
+	if player_mode == SpelerMode.SMALL:
+		is_dead = true
+		animated_sprite_2d.play("death")
+
+		area_2d.set_collision_layer_value(1, false)
+		area_2d.set_collision_mask_value(3, false)
+		set_collision_layer_value(1, false)
+		set_collision_mask_value(3, false)
+
+		set_physics_process(false)
+
+		var death_tween = get_tree().create_tween()
+		death_tween.tween_property(self, "position", position + Vector2(0, -48), 0.5)
+		death_tween.chain().tween_property(self, "position", position + Vector2(0, 256), 1)
+		death_tween.tween_callback(Callable(self, "_on_death_tween_complete"))
+
+func _on_death_tween_complete():
+	get_tree().reload_current_scene()
